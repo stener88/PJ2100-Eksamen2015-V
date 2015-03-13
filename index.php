e8bf1a8f2f8e8f913a6e4ea6fa601c9254a6712b
valmir95 <valmirmemeti@gmail.com>
2015-03-13 11:03:03 +0100

    Create index.php
    "Forsiden" av siden. En "text-field" av type text blir opprettet med funksjonen "post". Funksjonen blir hentet av filen "create.php"

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include 'connection.php';
+?>
+
+<h1> Submit email address </h1>
+
+<form action = "create.php" method = "post">
+	<input type ="text" name = "email" value = "" />
+	<br />
+
+	<input type = "submit" name = "submit" />
+
+</form>
